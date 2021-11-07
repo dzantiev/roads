de91dd6f4e8680c2d9dde8b15b197654e00ce42b
illi-homz <gopkabel@mail.ru>
2021-11-07 19:53:41 +0300

    set tests

--- e2e/units/sendCode.js
+++ e2e/units/sendCode.js
@@ -1,6 +1,8 @@
 export default async (code) => {
 	await element(by.id('codeNumber')).typeText(code);
-	console.log('code inputed')
-	// await element(by.id('MainLogo')).tap();
-	// await element(by.id('codeBtn')).tap();
+	console.log('code inputed');
+	await element(by.id('MainLogo')).tap();
+	console.log('main ligo pressed');
+	await element(by.id('codeBtn')).tap();
+	console.log('send code btn pressed')
 }
