60d72afc84e047b5c12cce1c6e881e161a4d81fd
illi-homz <gopkabel@mail.ru>
2021-11-07 11:55:03 +0300

    fix send code test

--- e2e/units/sendCode.js
+++ e2e/units/sendCode.js
@@ -1,5 +1,6 @@
 export default async (code) => {
 	await element(by.id('codeNumber')).typeText(code);
-	await element(by.id('MainLogo')).tap();
+	// await element(by.id('MainLogo')).tap();
+	await element(by.id('codeBtn')).tap();
 	await element(by.id('codeBtn')).tap();
 }
